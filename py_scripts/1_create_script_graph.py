129960b033a7b6c660430a303c29443ce45aa575
Lorenzo Stucchi <lorenzo.stucchi@polimi.it>
2022-05-06 15:57:54 +0200

    fix script 1 for dias

--- py_scripts/1_create_script_graph.py
+++ py_scripts/1_create_script_graph.py
@@ -1,36 +1,37 @@
 import os
 import json
-path_param = "input\\parameters.json"
-path_file_path_s2 = "input\\s2_paths.txt"
-path_file_path_s3 = "input\\s3_paths.txt"
-path_bandmask = "graph\\BandMaths_mask_sentinel_2_pre_processing.xml"
-path_bio = "graph\\BiophysicalOp_sentinel_2_pre_processing.xml"
-path_refl = "graph\\reflectance_sentinel_2_pre_processing.xml"
-path_resample = "graph\\resample_sentinel_2_pre_processing.xml"
-path_zen = "graph\\sun_zenith_sentinel_2_pre_processing.xml"
-path_LC = "graph\\add_landcover.xml"
-path_ele = "graph\\add_elevation.xml"
-path_cut = "graph\\sentinel_3_cut.xml"
-path_s3_pro = "graph\\sentinel_3_pre_processing_fix.xml"
-path_graph = "output\\graph\\"
-path_date_s2 = "output\\days_s2.txt"
-path_date_s3 = "output\\days_s3.txt"
-path_script_s3 = "output\\1_script_gpt_s3.sh"
-path_script_s2 = "output\\2_script_gpt_s2.sh"
+path_param = "input/parameters.json"
+path_file_path_s2 = "input/s2_paths.txt"
+path_file_path_s3 = "input/s3_paths.txt"
+path_bandmask = "graph/BandMaths_mask_sentinel_2_pre_processing.xml"
+path_bio = "graph/BiophysicalOp_sentinel_2_pre_processing.xml"
+path_refl = "graph/reflectance_sentinel_2_pre_processing.xml"
+path_resample = "graph/resample_sentinel_2_pre_processing.xml"
+path_zen = "graph/sun_zenith_sentinel_2_pre_processing.xml"
+path_LC = "graph/add_landcover.xml"
+path_ele = "graph/add_elevation.xml"
+path_cut = "graph/sentinel_3_cut.xml"
+path_s3_pro = "graph/sentinel_3_pre_processing_fix.xml"
+path_graph = "output/graph/"
+path_date_s2 = "output/days_s2.txt"
+path_date_s3 = "output/days_s3.txt"
+path_script_s3 = "output/1_script_gpt_s3.sh"
+path_script_s2 = "output/2_script_gpt_s2.sh"
 
 # Create directory
 if os.path.isdir("output") == False:
     os.mkdir("output")
-    os.mkdir("output\graph")
+    os.mkdir("output/graph")
 else:
-    if os.path.isdir("output\graph") == False:
-        os.mkdir("output\graph")
+    if os.path.isdir("output/graph") == False:
+        os.mkdir("output/graph")
 
 # Definition of general varibles
 with open(path_param, "r") as f:
     param = json.load(f)
 AOI_WTK = "POLYGON ((" + str(param["AOI"]["west"]) + " " + str(param["AOI"]["north"]) + ", " + str(param["AOI"]["est"]) + " " + str(param["AOI"]["north"]) + ", " + str(param["AOI"]["est"]) + " " + str(param["AOI"]["south"]) + ", " + str(param["AOI"]["west"]) + " " + str(param["AOI"]["south"]) + ", " + str(param["AOI"]["west"]) + " " + str(param["AOI"]["north"]) + ", " + str(param["AOI"]["west"]) + " " + str(param["AOI"]["north"]) + "))"
 general_path = param["general_path"]
+intermediate_output_path = param["temp_files"]
 
 # Save xml files to variable
 f = open(path_bandmask, "r")
@@ -67,27 +68,26 @@ script_text = ""
 date_s3 = ""
 for line in file_path_s3:
     s3_image_path = line.strip()
-    path = "\\".join(s3_image_path.split("\\")[:-1])
-    date = " ".join(s3_image_path.split("\\")[-2:-1])
+    path = intermediate_output_path + "/" + "/".join(s3_image_path.split("/")[-7:-1])
+    date = "_".join(s3_image_path.split("/")[-4:-1])
     date_s3 = date_s3 + date + "\n"
-    
-    s3_image_path = s3_image_path + "\\xfdumanifest.xml"
+    s3_image_path = s3_image_path + "/xfdumanifest.xml"
 
     # Cut
     script_text = script_text + "echo \"\t Cut the image S3 " + date + "\"\n"
-    cut_image = path + "\\S3_" + date + "_cut.dim"
+    cut_image = path + "/S3_" + date + "_cut.dim"
     text_cut = graph_cut.replace("!INPUT_Sentinel-3_LST!", s3_image_path).replace("!INPUT_AOI_WKT!", AOI_WTK).replace("!OUTPUT_Sentinel-3_cut!", cut_image)
     path_cut = path_graph + "sentinel_3_cut_"  + date +".xml"
     script_text = script_text + "gpt " + path_cut + "\n"
     f = open(path_cut, "w")
     f.write(text_cut)
     f.close()
-    
+
     # Processing
     script_text = script_text + "echo \"\t Processing the image S3 " + date + "\"\n"
-    obs_image = path + "\\S3_" + date + "_obs_geometry.dim"
-    mask_image = path + "\\S3_" + date + "_mask.dim"
-    lst_image = path + "\\S3_" + date + "_lst.dim"
+    obs_image = path + "/S3_" + date + "_obs_geometry.dim"
+    mask_image = path + "/S3_" + date + "_mask.dim"
+    lst_image = path + "/S3_" + date + "_lst.dim"
     text_s3_pro = graph_s3_pro.replace("!INPUT_Sentinel-3_LST!", cut_image).replace("!INPUT_AOI_WKT!", AOI_WTK).replace("!OUTPUT_observation_geometry!", obs_image).replace("!OUTPUT_mask!", mask_image).replace("!OUTPUT_LST!", lst_image)
     path_s3_pro = path_graph + "sentinel_3_preprocessing_fix_"  + date +".xml"
     script_text = script_text + "gpt " + path_s3_pro + "\n" + "\n\n"
@@ -111,16 +111,17 @@ script_text = ""
 date_s2 = ""
 for line in file_path_s2:
     s2_image_path = line.strip()
-    path = "\\".join(s2_image_path.split("\\")[:-1])
-    date = " ".join(s2_image_path.split("\\")[-2:-1])
+    tile = s2_image_path.split("/")[-1].split("_")[-2]
+    path = intermediate_output_path + "/" + "/".join(s2_image_path.split("/")[-7:-1]) + "/" + tile 
+    date = "_".join(s2_image_path.split("/")[-4:-1])
     date_s2 = date_s2 + date + "\n"
-    
-    sensor_s2 = " ".join(s2_image_path.split("\\")[-1:])[:3]
-    s2_image_path = s2_image_path + "\\MTD_MSIL2A.xml"
+
+    sensor_s2 = " ".join(s2_image_path.split("/")[-1:])[:3]
+    s2_image_path = s2_image_path + "/MTD_MSIL2A.xml"
     
     # Mask file
     script_text = script_text + "echo \"\t Computing the mask for the image S2 " + date + "\"\n"
-    mask_image = path + "\\S2_" + date + "_mask.dim"
+    mask_image = path + "/S2_" + date + "_mask.dim"
     text_mask = graph_bandmask.replace("!INPUT_Sentinel-2_L2A!", s2_image_path).replace("!OUTPUT_mask!", mask_image)
     path_mask = path_graph + "BandMaths_mask_sentinel_2_pre_processing_"  + date +".xml"
     script_text = script_text + "gpt " + path_mask + "\n"
@@ -130,7 +131,7 @@ for line in file_path_s2:
     
     # Biophysical
     script_text = script_text + "echo \"\t Computing the biophysical parameters for the image S2 " + date + "\"\n"
-    bio_image = path + "\\S2_" + date + "_biophysical.dim"
+    bio_image = path + "/S2_" + date + "_biophysical.dim"
     text_bio = graph_bio.replace("!INPUT_Sentinel-2_L2A!", s2_image_path).replace("!OUTPUT_biophysical!", bio_image).replace("!INPUT_Sentinel-2_Sensor!", sensor_s2)
     path_bio = path_graph + "BiophysicalOp_sentinel_2_pre_processing_"  + date +".xml"
     script_text = script_text + "gpt " + path_bio + "\n"
@@ -140,7 +141,7 @@ for line in file_path_s2:
     
     # Reflectance
     script_text = script_text + "echo \"\t Computing the reflectance for the image S2 " + date + "\"\n"
-    refl_image = path + "\\S2_" + date + "_reflectance.dim"
+    refl_image = path + "/S2_" + date + "_reflectance.dim"
     text_refl = graph_refl.replace("!INPUT_Sentinel-2_L2A!", s2_image_path).replace("!OUTPUT_reflectance!", refl_image)
     path_refl = path_graph + "reflectance_sentinel_2_pre_processing_"  + date +".xml"
     script_text = script_text + "gpt " + path_refl + "\n"
@@ -150,7 +151,7 @@ for line in file_path_s2:
     
     # Resample
     script_text = script_text + "echo \"\t Resampling the image S2 " + date + "\"\n"
-    resample_image = path + "\\S2_" + date + "_resampled.dim"
+    resample_image = path + "/S2_" + date + "_resampled.dim"
     text_resample = graph_resample.replace("!INPUT_Sentinel-2_L2A!", s2_image_path).replace("!OUTPUT_resampled!", resample_image)
     path_resample = path_graph + "resample_sentinel_2_pre_processing_"  + date +".xml"
     script_text = script_text + "gpt " + path_resample + "\n"
@@ -160,7 +161,7 @@ for line in file_path_s2:
     
     # Sun Zenith
     script_text = script_text + "echo \"\t Computing the sun zenith angular map for the image S2 " + date + "\"\n"
-    zen_image = path + "\\S2_" + date + "_sun_zenith_angle.dim"
+    zen_image = path + "/S2_" + date + "_sun_zenith_angle.dim"
     text_zen = graph_zen.replace("!INPUT_Sentinel-2_L2A!", s2_image_path).replace("!OUTPUT_sun_zenith_angle!", zen_image)
     path_zen = path_graph + "sun_zenith_sentinel_2_pre_processing_"  + date +".xml"
     script_text = script_text + "gpt " + path_zen + "\n"
@@ -170,7 +171,7 @@ for line in file_path_s2:
     
     # Landcover
     script_text = script_text + "echo \"\t Computing the landcover for the image S2 " + date + "\"\n"
-    LC_image = path + "\\S2_" + date + "_landcover.dim"
+    LC_image = path + "/S2_" + date + "_landcover.dim"
     text_LC = graph_LC.replace("!INPUT_Sentinel-2_Mask!", mask_image).replace("!OUTPUT_CCI_landcover!", LC_image)
     path_LC = path_graph + "add_landcover_"  + date +".xml"
     script_text = script_text + "gpt " + path_LC + "\n"
@@ -180,7 +181,7 @@ for line in file_path_s2:
 
     # Elevation
     script_text = script_text + "echo \"\t Computing the elevation for the image S2 " + date + "\"\n"
-    ele_image = path + "\\S2_" + date + "_elevation.dim"
+    ele_image = path + "/S2_" + date + "_elevation.dim"
     text_ele = graph_ele.replace("!INPUT_Sentinel-2_Mask!", mask_image).replace("!OUTPUT_SRTM_elevation!", ele_image)
     path_ele = path_graph + "add_elevation_"  + date +".xml"
     script_text = script_text + "gpt " + path_ele + "\n" +"\n\n"
