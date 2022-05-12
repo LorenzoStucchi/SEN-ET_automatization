3422e5a4590f487f2e41a9a58a6aa428b4bf3518
Lorenzo Stucchi <lorenzo.stucchi@polimi.it>
2022-05-12 14:41:06 +0200

    adapt main for dias

--- main.sh
+++ main.sh
@@ -2,15 +2,15 @@
 
 time python py_scripts/0_path_read.py
 echo "==================="
-time python py_scripts/1_create_script_graph.py
+time python3 py_scripts/1_create_script_graph.py
 echo "==================="
 time sh output/1_script_gpt_s3.sh
 echo "==================="
-time python py_scripts/2_read_times_s3.py
+time python3 py_scripts/2_read_times_s3.py
 echo "==================="
 time sh output/2_script_gpt_s2.sh
 echo "==================="
-time python py_scripts/3_create_script_python.py
+time python3 py_scripts/3_create_script_python.py
 echo "==================="
 time sh output/3_script_S2.sh
 echo "==================="
