49997f6b6467a837a1e2ba6f24e0d970c31c1030
Lorenzo Stucchi <lorenzo.stucchi@polimi.it>
2022-07-04 11:39:03 +0200

    remove time option on main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-time python3 py_scripts/0_path_read.py
+python3 py_scripts/0_path_read.py
 echo "==================="
-time python3 py_scripts/1_create_script_graph.py
+python3 py_scripts/1_create_script_graph.py
 echo "==================="
-time sh output/0_script_download_era.sh
+sh output/0_script_download_era.sh
 echo "==================="
-time sh output/1_script_gpt_s3.sh
+sh output/1_script_gpt_s3.sh
 echo "==================="
-time python3 py_scripts/2_read_times_s3.py
+python3 py_scripts/2_read_times_s3.py
 echo "==================="
-time sh output/2_script_gpt_s2.sh
+sh output/2_script_gpt_s2.sh
 echo "==================="
-time python3 py_scripts/3_create_script_python.py
+python3 py_scripts/3_create_script_python.py
 echo "==================="
-time sh output/3_script_S2.sh
+sh output/3_script_S2.sh
 echo "==================="
-time sh output/4_script_S3.sh
+sh output/4_script_S3.sh
 #read -r -p "Computation ended sucessfully! Press any key to close the panel" input
